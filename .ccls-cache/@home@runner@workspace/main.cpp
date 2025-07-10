74786466f567bca942402edd7efb721702ff313c
QGGB07 <qgb23300@email.vccs.edu>
2025-07-10 23:26:33 +0000

    Final Code

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -9,11 +9,19 @@ int main() {
     string firstName, lastName;
     int count = 0;
 
+    // Check if the file opens successfully
+    if (!inputFile) {
+        cout << "ERROR: Could not open LineUp.txt" << endl;
+        return 1;
+    }
+
+    // Read the first name from the file
     if (inputFile >> name) {
         firstName = name;
         lastName = name;
         count = 1;
 
+        // Keep reading the rest of the names
         while (inputFile >> name) {
             count++;
             if (name < firstName) {
@@ -28,7 +36,7 @@ int main() {
         cout << "First in line: " << firstName << endl;
         cout << "Last in line: " << lastName << endl;
     } else {
-        cout << "The file is empty or missing." << endl;
+        cout << "The file is empty." << endl;
     }
 
     inputFile.close();
